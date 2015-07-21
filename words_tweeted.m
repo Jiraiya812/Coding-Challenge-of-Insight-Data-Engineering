9186ac45c02bf91e1362ef39705edd23d1373ff9
Jiraiya812 <jiraiya812@gmail.com>
2015-07-21 02:22:06 -0400

    Update and rename haha to words_tweeted.m

---
+++ words_tweeted.m
@@ -0,0 +1,47 @@
+%%  words_tweeted.m
+% File Description
+% Auther: Yi Shan 
+% Location: University of Waterloo, Ontario, Canada
+% Email : y9shan@uwaterloo.ca
+% Date of Last Edit: 2015/07/20
+
+% File 
+%%
+tic;
+clc;clear all;
+InputDirectory='tweet_input/';
+IutputFilename='tweets1000.txt';
+
+fprintf(['The input tweet file name is:' IutputFilename '\n']);
+fprintf(' words_tweeted is running...\n');
+
+OntputDirectory='tweet_output/';
+OutputFilename_1='ft1.txt';
+OutputFilename_2='ft2.txt';
+
+
+%% read the tweets
+fid_input = fopen([InputDirectory IutputFilename],'r');
+words = textscan(fid_input, '%s');
+
+
+%% generate the unique wordlist and sort it 
+unique_words = unique(words{1,1});
+status = fclose(fid_input);
+words=words{1,1};
+
+%% count the frequency
+for i=1:length(unique_words)
+[x,y] = find(strcmp(words,unique_words{i}));
+frequency(i,1)=length(x);
+end
+
+%% Write the file
+fid_output=fopen([OntputDirectory OutputFilename_1],'wt');
+ for i=1:length(unique_words)
+ fprintf(fid_output,'%-60s  %g \n', unique_words{i} ,frequency(i,1)); 
+ end
+ fclose(fid_output);
+fprintf(' words_tweeted done, check folder /tweet_output/ft1.txt \n');
+toc;
+
